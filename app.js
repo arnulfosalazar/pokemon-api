c5fc53856ef8b481f13358e0fad7aa2e342ae217
arnulfosalazar <arnulfosalazar2001@gmail.com>
2026-09-19 07:24:15 -0500

    Added user input and dynamic API URL

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 async function main() {
+    const readline = require("node:readline/promises");
 
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/9999");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    const pokemonID = await rl.question("Enter Pokemon ID: ");
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
+    console.log(response.status);
+    if(!response.ok){
+        console.log("Request failed");
+        rl.close();
+        return 1;
+    }
     const data = await response.json();
     console.log(`Name: ${data.name}, Weight: ${data.weight}, Height: ${data.height}`);
-    console.log(response.status);
+    //console.log(data);
+
+    rl.close();
 }
 
 main();
